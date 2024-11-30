590363971d3b5d397636e1b988b3a2c5306e756f
mingovvv <mingovvv@gmail.com>
2024-12-01 00:03:03 +0900

    Feat : "2024-12-01"

---
+++ src/main/java/advenced/java/AdvencedJavaApplication.java
@@ -0,0 +1,13 @@
+package advenced.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvencedJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvencedJavaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/advenced/java/spring/v2/OrderServiceV2.java
@@ -0,0 +1,27 @@
+package advenced.java.spring.v2;
+
+import advenced.java.spring.trace.TraceId;
+import advenced.java.spring.trace.TraceStatus;
+import advenced.java.spring.trace.v1.TraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final TraceV2 trace;
+    public void orderItem(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(traceId, itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
